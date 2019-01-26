b57d6159f9c72f55f8857f2868dc329a72a52f60
Prateek Vasireddy <prat.vasireddy@gmail.com>
2019-01-25 20:20:38 -0500

    add more

--- routes/botman.php
+++ routes/botman.php
@@ -5,5 +5,5 @@ $botman = resolve('botman');
 
 // listen for an Alexa Skill Intent Name
 $botman->hears('SearchConferences', function ($bot) {
-    $bot->reply('These are your upcoming conferences: Laracon 2019');
+    $bot->reply('These are your upcoming conferences: Laracon 2019, RailsConf 2019');
 });
